ce377d029d7f96f88b7a61f22ecda16d57548d27
ACHYUTHAUDAYNAVYAKRISHNA <162981177+ACHYUTHAUDAYNAVYAKRISHNA@users.noreply.github.com>
2024-03-11 15:57:25 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,22 @@
+#print("hello world")
+'''
+print("hello world")
+print("hello world")
+'''
+print("hello world")
+print("hello world")
+
+
+
+
+
+
+#import this
+#print this
+import antigravity
+
+
+
+
+
+   
