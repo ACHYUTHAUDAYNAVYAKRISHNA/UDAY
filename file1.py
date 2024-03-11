889e6fa563aabf5955fdb0a79c1eebe477fc6188
ACHYUTHAUDAYNAVYAKRISHNA <162981177+ACHYUTHAUDAYNAVYAKRISHNA@users.noreply.github.com>
2024-03-11 16:04:12 +0530

    Add files via upload

--- file1.py
+++ file1.py
@@ -1,3 +1,32 @@
+'''
+class.1 
+Bhargained  charecters.
+
+
+* strings and its formates- from c.
+* regular expressions - pearl.
+* exception handling- module3.
+* Magic methods from - fortune.
+
+what kind of language is python.
+
+ python is
+ 
+* object orinted programming language.
+*  dynamically typed language.
+* scripting language.
+* inter preted language.
+* compiled language.
+* function programming lang language.
+
+flavours of python.
+
+* cpython - written in c, most common implementation of python.
+* jython -java implimentation of the python programming language which is tightly integrested with.NET. 
+* brython - browser python,runs in browsers
+* Rubypython-bridge between python and ruby interpreters
+* micropython-runs in micro controles .
+'''
 #print("hello world")
 '''
 print("hello world")
@@ -5,8 +34,11 @@ print("hello world")
 '''
 print("hello world")
 print("hello world")
-
-
+'''
+1 commenting is used for making documentation in a pyhon file.
+2 when the line is commented, that will not be considerd as a part of the code.
+3 when the program is executed commented line will not be in the part of execution.
+'''
 
 
 
@@ -14,6 +46,66 @@ print("hello world")
 #import this
 #print this
 import antigravity
+
+# ---> print(object, sep, end, file, flush)
+#sep--> seperator
+#Eg
+#a=6
+#b=4
+#c=5
+#print(a,b,c sep='$')
+
+#end --.which is used to add string at the of the value,
+#it will not keep the command to stay in same line
+#Eg:
+#print(str(a),b, end='%%'
+#print(str(c))
+
+
+
+
+
+
+
+# ! ----> variables
+# variable is a name which is used to identify the value of variable
+# ?which is stored in a specific memory location 
+
+
+
+# ! Rules to define variables
+# 1)variable have to start with only alphabets, underscores
+# 2)the char following the 1st char can be alphabets, numbers, underscores
+# 3)no spaces are allowded
+# 4)no specioal charcters like !@#$%^&*()+
+# 5)case sensitive
+# 6) keywords are not allowed as variables
+
+
+
+
+# !ways to define variables
+
+
+# a= 8
+# b=9
+#print(a,b)
+
+# a=5,6, 9.46, "hello"
+# print(a)
+# print(type(a))
+
+
+
+#a,b =
+
+
+
+
+
+
+       
+
 
 
 
